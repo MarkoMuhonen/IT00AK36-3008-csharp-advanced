9de4d876b2a2495ef25924aefdc433bfd9aa1af3
Marko Muhonen <marko.muhonen@centria.fi>
2026-03-08 11:30:29 +0200

    First functionalities + test code ready

--- Harjoitusprojekti2/project2.cs
+++ Harjoitusprojekti2/project2.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 
-    class Student
+class Student
     {
         public string Name;
         public int Age;
@@ -27,6 +28,14 @@ using System.Collections.Generic;
             List<Student> students = new List<Student>();
 
             // TODO: Add initial students here
+            AddStudent(students, new Student("Jorma Kemppainen", 22, "Putkitekniikka"));
+            AddStudent(students, new Student("Keijo Vilunki", 22, "Taloustiede"));
+            AddStudent(students, new Student("Raija Rätinki", 19, "Taloushallinto"));
+            AddStudent(students, new Student("Anna", 20, "ICT"));
+            AddStudent(students, new Student("Mikko", 23, "Business"));
+            AddStudent(students, new Student("Liisa", 21, "ICT"));
+            AddStudent(students, new Student("Pekka", 25, "Engineering"));
+
 
             PrintAllStudents(students);
 
@@ -36,11 +45,22 @@ using System.Collections.Generic;
         static void AddStudent(List<Student> students, Student student)
         {
             // TODO
+            students.Add(student);
+            Console.WriteLine("Student added: " + student);
         }
 
         static void FindStudentByName(List<Student> students, string name)
         {
             // TODO
+            foreach (Student student in students)
+            {
+                if (student.Name == name)
+                {
+                    Console.WriteLine(student);
+                    return;
+                }
+            }
+            Console.WriteLine("Student not found.");
         }
 
         static void PrintStudentsOlderThan(List<Student> students, int age)
